f1db0047c2b4dbb59bb3ea28f4421c312e03fa48
Welperooni <Welperooni@gmail.com>
2018-06-19 02:15:44 +0200

    Added a doubly linked list
    wasn't able to make it just two pointers to the back and the front as I've encountered a segfault wall

--- DLL-ptr.hs
+++ DLL-ptr.hs
@@ -8,57 +8,84 @@ import Control.Lens
 import System.IO.Unsafe
 import Generics.Deriving
 
-data DLL a = DLL { 
-    _backw :: Ptr (DLL a), 
+data DLN a = DLN { 
+    _backw :: Ptr (DLN a), 
     _this :: Ptr a, 
-    _thisPtr :: Ptr (DLL a),
-    _forw :: Ptr (DLL a)
+    _thisPtr :: Ptr (DLN a),
+    _forw :: Ptr (DLN a)
   } deriving Generic
 
-firstElem :: Storable a => DLL a -> DLL a  
-firstElem nd = if _backw nd /= nullPtr then 
-    firstElem $ unsafePerformIO (peek $ _backw nd) 
+makeLenses ''DLN
+instance Storable a => GStorable (DLN a)
+
+firstElemDLN :: Storable a => DLN a -> DLN a  
+firstElemDLN nd = if _backw nd /= nullPtr then 
+    firstElemDLN $ unsafePerformIO (peek $ _backw nd) 
   else nd
 
-lastElem :: Storable a => DLL a -> DLL a
-lastElem nd = if _forw nd /= nullPtr then 
-    lastElem $ unsafePerformIO (peek $ _forw nd) 
+lastElemDLN :: Storable a => DLN a -> DLN a
+lastElemDLN nd = if _forw nd /= nullPtr then 
+    lastElemDLN $ unsafePerformIO (peek $ _forw nd) 
   else nd
   
-instance (Storable a, Show a) => Show (DLL a) where
+instance (Storable a, Show a) => Show (DLN a) where
   show l = 
-    let accumEls :: Storable a => DLL a -> [a]
+    let accumEls :: Storable a => DLN a -> [a]
         accumEls e 
           |  _forw e == nullPtr = unsafePerformIO (peek $ _this e) : []
           | otherwise = unsafePerformIO (peek $ _this e) : accumEls (unsafePerformIO (peek $ _forw e))
-    in show $ accumEls (firstElem l)    
+    in show $ accumEls (firstElemDLN l)
+
+data DLL a = DLL {
+    _front :: (DLN a),
+    _back :: (DLN a)
+  } deriving Generic
 
 makeLenses ''DLL
 instance Storable a => GStorable (DLL a)
 
-singletonDLL :: Storable a => a -> DLL a
-singletonDLL a = 
+instance (Storable a, Show a) => Show (DLL a) where
+  show (DLL fr _) = show fr
+
+singletonDLN :: Storable a => a -> DLN a
+singletonDLN a = 
   let ptr = unsafePerformIO malloc 
       pval = unsafePerformIO malloc
       val = unsafePerformIO (poke pval a)
-  in  ptr `seq` val `seq` DLL nullPtr pval ptr nullPtr
+  in  ptr `seq` val `seq` DLN nullPtr pval ptr nullPtr
 
-consDLL :: Storable a => a -> DLL a -> DLL a 
-consDLL e (firstElem -> l) = 
-  let ptr = unsafePerformIO malloc
-      pval = unsafePerformIO malloc
-      val = unsafePerformIO (poke pval e)
-      dll = DLL nullPtr pval ptr (_thisPtr l) 
-      mem = unsafePerformIO (poke ptr dll)
-      newNext = unsafePerformIO $ poke (_thisPtr l) (backw .~ ptr $ l)
-  in  val `seq` mem `seq` newNext `seq` dll 
+singletonDLL :: Storable a => a -> DLL a
+singletonDLL a =
+  let node = singletonDLN a
+  in DLL node node
+
+consDLL :: Storable a => a -> DLL a -> DLL a
+consDLL e dll@(DLL fs lt) =
+  let newN = consDLN e fs
+  in  newN `seq` (dll & front .~ newN & back .~ lastElemDLN newN)
 
 snocDLL :: Storable a => DLL a -> a -> DLL a
-snocDLL (lastElem -> nd) e =
+snocDLL dll@(DLL fs lt) e =
+  let newN = snocDLN lt e
+  in  newN `seq` (dll & back .~ newN & front .~ firstElemDLN newN)
+
+consDLN :: Storable a => a -> DLN a -> DLN a 
+consDLN e (firstElemDLN -> l) = 
   let ptr = unsafePerformIO malloc
       pval = unsafePerformIO malloc
       val = unsafePerformIO (poke pval e)
-      dll = DLL (_thisPtr nd) pval ptr nullPtr
-      mem = unsafePerformIO (poke ptr dll)
+      dln = DLN nullPtr pval ptr (_thisPtr l) 
+      mem = unsafePerformIO (poke ptr dln)
+      newNext = unsafePerformIO $ poke (_thisPtr l) (backw .~ ptr $ l)
+  in  val `seq` mem `seq` newNext `seq` dln 
+
+snocDLN :: Storable a => DLN a -> a -> DLN a
+snocDLN (lastElemDLN -> nd) e =
+  let ptr = unsafePerformIO malloc
+      pval = unsafePerformIO malloc
+      val = unsafePerformIO (poke pval e)
+      dln = DLN (_thisPtr nd) pval ptr nullPtr
+      mem = unsafePerformIO (poke ptr dln)
       newPrev = unsafePerformIO $ poke (_thisPtr nd) (forw .~ ptr $ nd)
-  in  val `seq` mem `seq` newPrev `seq` dll
+  in  val `seq` mem `seq` newPrev `seq` dln
+
